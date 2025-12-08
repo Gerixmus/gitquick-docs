25f6ff9849933021695fa0ceba4073fad884350a
Gerixmus <gerixmuss@gmail.com>
2025-12-08 23:37:55 +0100

    docs: add rust logo

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -31,12 +31,12 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Powered by Rust',
+    Svg: require('@site/static/img/rust-logo-blk.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Suspendisse nec gravida mi, vel efficitur ex.
       </>
     ),
   },
